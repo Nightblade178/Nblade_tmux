c61961f3dd6a559341bbd64271728c4ea86330cd
Thakurdeep Singh <thakurdeep1789@gmail.com>
2026-07-21 18:45:15 -0400

    Added linking instead of cp

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ mkdir -p ~/.config/tmux/plugins/catppuccin ~/.config/tmux/plugins/tmux-plugins/{
 git clone -b v2.3.0 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
 git clone https://github.com/tmux-plugins/tmux-battery ~/.config/tmux/plugins/tmux-plugins/tmux-battery/
 git clone https://github.com/tmux-plugins/tmux-cpu ~/.config/tmux/plugins/tmux-plugins/tmux-cpu/
-cp .tmux.conf ~/.tmux.conf
+rm ~/.tmux.conf
+ln -s $(realpath .tmux.conf) ~/.tmux.conf
