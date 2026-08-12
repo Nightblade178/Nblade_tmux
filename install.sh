abf431386dd7af207aad0cc4fb70f70f02f107b1
Thakurdeep Singh <thakurdeep.singh@netaccess.ca>
2026-08-12 12:38:36 -0400

    added uninstall and reinstall

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+sudo apt purge tmux
+sudo apt install tmux -y
 mkdir -p ~/.config/tmux/plugins/catppuccin ~/.config/tmux/plugins/tmux-plugins/{tmux-battery,tmux-cpu}
+rm -rf ~/.config/tmux/plugins/catppuccin/tmux
+rm -rf ~/.config/tmux/plugins/tmux-plugins/tmux-battery/
+rm -rf ~/.config/tmux/plugins/tmux-plugins/tmux-cpu/
+rm -rf ~/.tmux/plugins/tpm
 git clone -b v2.3.0 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
 git clone https://github.com/tmux-plugins/tmux-battery ~/.config/tmux/plugins/tmux-plugins/tmux-battery/
 git clone https://github.com/tmux-plugins/tmux-cpu ~/.config/tmux/plugins/tmux-plugins/tmux-cpu/
